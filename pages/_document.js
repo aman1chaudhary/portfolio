4084c6d35ec846c4f17147606242abbcfc01b096
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2023-08-18 23:34:51 +0530

    font6

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,13 @@ export default function Document() {
         <meta name="author" content="Aman Chaudhary" />
         <meta name="google-site-verification" content="MZTK0uf2XlaN0lQNV9tIfP56jnhWKCVj332CE7yK0PM" />
 
+
+
         <link rel="stylesheet" type="text/css" charset="UTF-8"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
         <link rel="stylesheet" type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
 
       </Head>
       <body>
