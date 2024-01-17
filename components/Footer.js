a6059f32823bbd9c4b9da911a61b524470e14c63
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2024-01-17 14:47:36 +0530

    17 jan

--- components/Footer.js
+++ components/Footer.js
@@ -18,7 +18,7 @@ class Footer extends Component {
                                     </h4>
                                     <ul className="list-inline m-a0 text-center">
                                         <li><a href="https://github.com/aman1chaudhary" target='_blank' rel="noreferrer" className="site-button facebook circle mr-1"><i className="fa fa-github"></i></a></li>
-                                        <li><a href="https://www.linkedin.com/in/aman-chaudhary-4a1716203/" target='_blank' rel="noreferrer"  className="site-button linkedin circle mr-1"><i className="fa fa-linkedin"></i></a></li>
+                                        <li><a href="https://www.linkedin.com/in/amanchaudhary-com/" target='_blank' rel="noreferrer"  className="site-button linkedin circle mr-1"><i className="fa fa-linkedin"></i></a></li>
                                         <li><a href="https://www.instagram.com/amanchaudhary.__/" target='_blank' rel="noreferrer"  className="site-button instagram circle mr-1"><i className="fa fa-instagram"></i></a></li>
                                         <li><a href="https://twitter.com/i/flow/login?redirect_after_login=%2FChaudharyAman__" target='_blank' rel="noreferrer"  className="site-button twitter circle mr-1"><i className="fa fa-twitter"></i></a></li>
                                         <li><a href="mailto:aman.chaudhary@iitgn.ac.in" className="site-button google-plus circle mr-1"><i className="fa fa-envelope"></i></a></li>
